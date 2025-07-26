6043209c5f4ed93e0581fc52193fc9308e31b9a0
nunezag816 <nunezag@msoe.edu>
2025-07-25 19:57:34 -0500

    Update Node.java

--- app/src/main/java/org/example/Node.java
+++ app/src/main/java/org/example/Node.java
@@ -1,31 +1,31 @@
 package org.example;
 
-public class Node {
-    private Squirrel data;
-    private Node left;
-    private Node right;
+public class Node<T> {
+    private T data;
+    private Node<T> left;
+    private Node<T> right;
 
-    public Node(Squirrel data) {
+    public Node(T data) {
         this.data = data;
     }
 
-    public Squirrel getData() {
+    public T getData() {
         return data;
     }
 
-    public void setLeft(Node node) {
+    public void setLeft(Node<T> node) {
         this.left = node;
     }
 
-    public void setRight(Node node) {
+    public void setRight(Node<T> node) {
         this.right = node;
     }
 
-    public Node left() {
+    public Node<T> left() {
         return left;
     }
 
-    public Node right() {
+    public Node<T> right() {
         return right;
     }
 }
